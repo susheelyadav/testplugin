5d6a71c6a76b95cbbeb2cf5c592c34af0ff01fc3
susheelyadav <97273694+susheelyadav@users.noreply.github.com>
2023-12-29 18:06:11 +0530

    Update Testplugin.h

--- src/ios/Testplugin.h
+++ src/ios/Testplugin.h
@@ -1,13 +1,8 @@
 #import <Foundation/Foundation.h>
 #import <Cordova/CDV.h>
-#import <ffmpegkit/FFmpegKitConfig.h>
-#import <ffmpegkit/FFmpegKit.h>
 
-@interface Testplugin : CDVPlugin
+@interface Testplugin : CDVPlugin {}
 
 - (void)mergeAudioFiles:(CDVInvokedUrlCommand *)command;
-- (NSString *)buildFFmpegCommandWithAudioFiles:(NSArray<NSString *> *)audioFiles outputFile:(NSString *)outputFile;
-- (NSString *)generateOutputFilePath;
-- (NSString *)createFileListWithAudioFiles:(NSArray<NSString *> *)audioFiles;
 
 @end
