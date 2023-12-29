6b2da3feeca7ef8a856c30180cda932fed241e68
susheelyadav <97273694+susheelyadav@users.noreply.github.com>
2023-12-29 15:50:56 +0530

    Update testplugin.m

--- src/ios/testplugin.m
+++ src/ios/testplugin.m
@@ -1,28 +1,8 @@
-/********* testplugin.m Cordova Plugin Implementation *******/
-
+#import <Foundation/Foundation.h>
 #import <Cordova/CDV.h>
 
-@interface testplugin : CDVPlugin {
-  // Member variables go here.
-}
+@interface testplugin : CDVPlugin {}
 
-- (void)coolMethod:(CDVInvokedUrlCommand*)command;
-@end
-
-@implementation testplugin
-
-- (void)coolMethod:(CDVInvokedUrlCommand*)command
-{
-    CDVPluginResult* pluginResult = nil;
-    NSString* echo = [command.arguments objectAtIndex:0];
-
-    if (echo != nil && [echo length] > 0) {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
-    } else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-    }
-
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-}
+- (void)mergeAudioFiles:(CDVInvokedUrlCommand *)command;
 
 @end
